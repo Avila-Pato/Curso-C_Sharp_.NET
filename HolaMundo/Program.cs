8bd0a2f2566c20e1a3d1d9774267c2a07955987e
Avila-Pato <p.avilaf1998@gmail.com>
2024-08-09 12:53:22 -0400

    Agregar archivos de proyecto.

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Precalentando el horno");
+            Console.WriteLine("2. Mezclar harina y mantequilla");
+            Console.WriteLine("3. agreagar azuacar y harina");
+            Console.WriteLine("4. Darles forma y colocar en charola");
+            Console.WriteLine("5. Hornear durante 20 minutos");
+
+            //Declaracionnde 3 variables tipo entero
+            int numeroLibros = 500, librosMujeres = 100, librosHombresñ = 100;
+
+            //Declaracion de variables tipo double y le asigno un valor
+            double promedioFinal = 9.3;
+
+
+            numeroLibros = 500;
+            char salon;
+            salon = 'A';
+            String saludo = "Hola";
+            bool x = true;
+            decimal promedioExamenes = 8.5M;
+            float PromedioExamenes2 = 8.3F;
+
+        }
+    }
+}
