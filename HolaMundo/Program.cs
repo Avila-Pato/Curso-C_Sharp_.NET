7e455119e02d612851403e534084351a5fd8b8c3
Avila-Pato <p.avilaf1998@gmail.com>
2024-08-09 13:12:39 -0400

    Variables

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -10,26 +10,16 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1. Precalentando el horno");
-            Console.WriteLine("2. Mezclar harina y mantequilla");
-            Console.WriteLine("3. agreagar azuacar y harina");
-            Console.WriteLine("4. Darles forma y colocar en charola");
-            Console.WriteLine("5. Hornear durante 20 minutos");
+            string saludo = "hola Mundo";
+            char caracter = 'a';
 
-            //Declaracionnde 3 variables tipo entero
-            int numeroLibros = 500, librosMujeres = 100, librosHombresñ = 100;
+            double i = 9.3;
 
-            //Declaracion de variables tipo double y le asigno un valor
-            double promedioFinal = 9.3;
+            Console.WriteLine(i);
+            Console.WriteLine(caracter);
 
+            Console.WriteLine(saludo);
 
-            numeroLibros = 500;
-            char salon;
-            salon = 'A';
-            String saludo = "Hola";
-            bool x = true;
-            decimal promedioExamenes = 8.5M;
-            float PromedioExamenes2 = 8.3F;
 
         }
     }
